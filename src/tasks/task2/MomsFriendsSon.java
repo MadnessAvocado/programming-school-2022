ddfeec7309776fe015e8d0408c4c7f4a77b85250
bash <MadnessAvocado@programming-school-server.ru-central1.internal>
2022-08-26 16:23:36 +0000

    Son in market

---
+++ src/tasks/task2/MomsFriendsSon.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class MomsFriendsSon(){
+    public void doShopping(){
+        boolean bread = this.getAnswerFromConsole("Are there a bread in a market? (Answer 'yes or 'no')");
+        boolean eggs = this.getAnswerFromConcole("Are there an eggs in a market? (Answer 'yes' or 'no')");
+        boolean milk = this.getAnswerFromConsole("Are there a milk in a market? (Answer 'yes' or 'no')");
+
+        if (bread) {
+            System.out.println("Getting bread");
+        } else {
+            System.out.println("Getting 2 bears");
+        }
+
+        if (eggs) {
+            System.out.println("Getting 10 eggs");
+        }
+
+        if (milk) {
+            System.out.println("Getting milk");
+        }
+
+    }
+    private boolean getAnswerFromConsole(String request) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println(request);
+        String answer = myObj.nextLine();
+
+        if (answer.equals("yes")) {
+            return true;
+        }
+        else if (answer.equals("no") {
+            return false;
+        }
+        else {
+            System.out.println("Answer should be 'yes' or 'no'!);
+            System.exit(1);
+        }
+                        
+    }
+    public void main() {
+        MomsFriendsSon momsFriendsSon = new MomsFriendsSon();
+        momsFriendsSon.doShopping();
+    }
+}
